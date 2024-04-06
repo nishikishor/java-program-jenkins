a87a9cc302a6e6fa5de3a2818a3332f335ae0939
nishikishor <nishikishore5@gmail.com>
2024-04-06 17:44:27 +0530

    Update Main.java
    add more changes

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("This is a java program");
     System.out.println("current date : " + new Date());
+    System.out.println("add more changes");
   }
 
 }
